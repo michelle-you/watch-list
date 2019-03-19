123709c039907f0d75332166d35d9fe2a61c3c56
Michelle You <michellecyou@gmail.com>
2019-03-19 14:51:22 -0700

    initial files

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var mysql = require('mysql');
+
+var app = express();
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+var port = 3000;
+app.listen(port);
+
+//create mysql connection
+
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password: 'Asdqqwe321!',
+    database:'movie_planner_DB'
+
+});
+
+connection.connect(function(err){
+    if(err)throw err;
+    console.log('Connected as id: ' + connection.threadID);
+})
